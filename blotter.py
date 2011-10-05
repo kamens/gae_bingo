cce8313760cfa203e4cdfa7eef68dec05c0cd958
Marcos Ojeda <subliminal@gmail.com>
2011-10-05 12:41:27 -0700

    allow alternative_params and conversion_name to be passed in
    additionally, clean up defaults to None and also allow to create tests

--- blotter.py
+++ blotter.py
@@ -27,9 +27,9 @@ class Blotter(RequestHandler):
     failed requests return 404 if the experiment is not found and 
       a return a 400 if the params are passed incorrectly
     """
-    experiment_name = self.request.get("canonical_name", "")
-    alternative_params = self.request.get("alternative_params", None)
-    conversion_name = self.request.get("conversion_name", None)
+    experiment_name = self.request.get("canonical_name", None)
+    alternative_params = self.request.get("alternative_params[]", allow_multiple=True, default_value = None)
+    conversion_name = self.request.get("conversion_name[]", allow_multiple=True, default_value = None)
 
     logging.info("alternative_params: %s", alternative_params)
 
@@ -38,10 +38,18 @@ class Blotter(RequestHandler):
     self.response.headers['Content-Type'] = 'text/json'
 
     # return false if experiment not found (don't create a new one!)
-    if(experiment_name is not ""):
+    if(experiment_name):
       if experiment_name not in bingo_cache.experiments:
-        self.response.set_status(404)
-        return
+        if can_control_experiments():
+          # create the given ab_test with passed params, etc
+          logging.info("creating experiment %s", experiment_name)
+          condition = str(ab_test(experiment_name, alternative_params, conversion_name))
+          self.response.out.write('"%s"' % (condition))
+          return
+        else:
+          # experiment not found (and not being created)
+          self.response.set_status(404)
+          return
       
       # return status for experiment (200 implicit)
       else:
@@ -68,13 +76,13 @@ class Blotter(RequestHandler):
     """
     bingo_cache, bingo_identity_cache = bingo_and_identity_cache()
     
-    conversion = self.request.get("convert","")
+    conversion = self.request.get("convert", None)
 
     self.response.headers['Content-Type'] = 'text/json'
 
     experiment_names = bingo_cache.get_experiment_names_by_conversion_name(conversion)
     logging.info(experiment_names)
-    if (conversion is not ""):
+    if (conversion):
       if(len(experiment_names) > 0):
         # send null message
         self.response.set_status(204)
